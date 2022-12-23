63d6ebe38b2b4d31932a227c72cb2c003751f916
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2022-12-23 16:38:08 -0700

    Move json schemas to jpo-ode-core/main/resources so they can be tested.  Unit test for bsm json validation.

--- jpo-ode-core/src/test/java/us/dot/its/jpo/ode/model/OdeMapDataTest.java
+++ jpo-ode-core/src/test/java/us/dot/its/jpo/ode/model/OdeMapDataTest.java
@@ -6,12 +6,12 @@ import static org.junit.Assert.*;
 import us.dot.its.jpo.ode.util.JsonUtils;
 
 public class OdeMapDataTest {
+
+    final String json = "{\"metadata\":{\"logFileName\":\"\",\"recordType\":\"mapTx\",\"securityResultCode\":\"success\",\"receivedMessageDetails\":{\"locationData\":null,\"rxSource\":\"NA\"},\"encodings\":null,\"payloadType\":\"us.dot.its.jpo.ode.model.OdeMapPayload\",\"serialId\":{\"streamId\":\"18d7c2e0-9158-4456-916d-5cd4b080d290\",\"bundleSize\":1,\"bundleId\":0,\"recordId\":0,\"serialNumber\":0},\"odeReceivedAt\":\"2022-06-17T19:02:13.083984Z\",\"schemaVersion\":6,\"maxDurationTime\":0,\"recordGeneratedAt\":\"\",\"recordGeneratedBy\":null,\"sanitized\":false,\"odePacketID\":\"\",\"odeTimStartDateTime\":\"\",\"mapSource\":\"RSU\",\"originIp\":\"10.11.81.25\"},\"payload\":{\"data\":{\"timeStamp\":null,\"msgIssueRevision\":2,\"layerType\":\"intersectionData\",\"layerID\":0,\"intersections\":null,\"roadSegments\":null,\"dataParameters\":null,\"restrictionList\":null},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735MAP\"}}";
+
     @Test
     public void shouldDeserializeJson() {
-
-        final String mapJson = "{\"metadata\":{\"logFileName\":\"\",\"recordType\":\"mapTx\",\"securityResultCode\":\"success\",\"receivedMessageDetails\":{\"locationData\":null,\"rxSource\":\"NA\"},\"encodings\":null,\"payloadType\":\"us.dot.its.jpo.ode.model.OdeMapPayload\",\"serialId\":{\"streamId\":\"18d7c2e0-9158-4456-916d-5cd4b080d290\",\"bundleSize\":1,\"bundleId\":0,\"recordId\":0,\"serialNumber\":0},\"odeReceivedAt\":\"2022-06-17T19:02:13.083984Z\",\"schemaVersion\":6,\"maxDurationTime\":0,\"recordGeneratedAt\":\"\",\"recordGeneratedBy\":null,\"sanitized\":false,\"odePacketID\":\"\",\"odeTimStartDateTime\":\"\",\"mapSource\":\"RSU\",\"originIp\":\"10.11.81.25\"},\"payload\":{\"data\":{\"timeStamp\":null,\"msgIssueRevision\":2,\"layerType\":\"intersectionData\",\"layerID\":0,\"intersections\":null,\"roadSegments\":null,\"dataParameters\":null,\"restrictionList\":null},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735MAP\"}}";
-
-        var deserialized = (OdeMapData)JsonUtils.fromJson(mapJson, OdeMapData.class);
+        final var deserialized = (OdeMapData)JsonUtils.fromJson(json, OdeMapData.class);
         assertNotNull(deserialized);
         assertTrue(deserialized.getMetadata() instanceof OdeMapMetadata);
         assertTrue(deserialized.getPayload() instanceof OdeMapPayload);
@@ -20,11 +20,8 @@ public class OdeMapDataTest {
 
     @Test
     public void serializationShouldNotAddClassProperty() {
-
-        final String mapJson = "{\"metadata\":{\"logFileName\":\"\",\"recordType\":\"mapTx\",\"securityResultCode\":\"success\",\"receivedMessageDetails\":{\"locationData\":null,\"rxSource\":\"NA\"},\"encodings\":null,\"payloadType\":\"us.dot.its.jpo.ode.model.OdeMapPayload\",\"serialId\":{\"streamId\":\"18d7c2e0-9158-4456-916d-5cd4b080d290\",\"bundleSize\":1,\"bundleId\":0,\"recordId\":0,\"serialNumber\":0},\"odeReceivedAt\":\"2022-06-17T19:02:13.083984Z\",\"schemaVersion\":6,\"maxDurationTime\":0,\"recordGeneratedAt\":\"\",\"recordGeneratedBy\":null,\"sanitized\":false,\"odePacketID\":\"\",\"odeTimStartDateTime\":\"\",\"mapSource\":\"RSU\",\"originIp\":\"10.11.81.25\"},\"payload\":{\"data\":{\"timeStamp\":null,\"msgIssueRevision\":2,\"layerType\":\"intersectionData\",\"layerID\":0,\"intersections\":null,\"roadSegments\":null,\"dataParameters\":null,\"restrictionList\":null},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735MAP\"}}";
-
-        var deserialized = (OdeMapData)JsonUtils.fromJson(mapJson, OdeMapData.class);
-        String serialized = JsonUtils.toJson(deserialized, false);
+        final var deserialized = (OdeMapData)JsonUtils.fromJson(json, OdeMapData.class);
+        final String serialized = deserialized.toJson(false);
         assertFalse(serialized.contains("@class"));
     }
 }
--- jpo-ode-core/src/test/java/us/dot/its/jpo/ode/model/OdeSpatDataTest.java
+++ jpo-ode-core/src/test/java/us/dot/its/jpo/ode/model/OdeSpatDataTest.java
@@ -6,12 +6,12 @@ import static org.junit.Assert.*;
 import us.dot.its.jpo.ode.util.JsonUtils;
 
 public class OdeSpatDataTest {
+    
+    final String json = "{\"metadata\":{\"logFileName\":\"\",\"recordType\":\"spatTx\",\"securityResultCode\":\"success\",\"receivedMessageDetails\":{\"locationData\":null,\"rxSource\":\"NA\"},\"encodings\":null,\"payloadType\":\"us.dot.its.jpo.ode.model.OdeSpatPayload\",\"serialId\":{\"streamId\":\"24d9dfcb-1ca0-41b8-8576-f4a00a51218e\",\"bundleSize\":1,\"bundleId\":0,\"recordId\":0,\"serialNumber\":0},\"odeReceivedAt\":\"2022-06-17T19:02:05.366148Z\",\"schemaVersion\":6,\"maxDurationTime\":0,\"recordGeneratedAt\":\"\",\"recordGeneratedBy\":null,\"sanitized\":false,\"odePacketID\":\"\",\"odeTimStartDateTime\":\"\",\"spatSource\":\"V2X\",\"originIp\":\"10.11.81.12\",\"isCertPresent\":false},\"payload\":{\"data\":{\"timeStamp\":null,\"name\":null,\"intersectionStateList\":null},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735SPAT\"}}";
+
     @Test
     public void shouldDeserializeJson() {
-
-        final String bsmJson = "{\"metadata\":{\"logFileName\":\"\",\"recordType\":\"spatTx\",\"securityResultCode\":\"success\",\"receivedMessageDetails\":{\"locationData\":null,\"rxSource\":\"NA\"},\"encodings\":null,\"payloadType\":\"us.dot.its.jpo.ode.model.OdeSpatPayload\",\"serialId\":{\"streamId\":\"24d9dfcb-1ca0-41b8-8576-f4a00a51218e\",\"bundleSize\":1,\"bundleId\":0,\"recordId\":0,\"serialNumber\":0},\"odeReceivedAt\":\"2022-06-17T19:02:05.366148Z\",\"schemaVersion\":6,\"maxDurationTime\":0,\"recordGeneratedAt\":\"\",\"recordGeneratedBy\":null,\"sanitized\":false,\"odePacketID\":\"\",\"odeTimStartDateTime\":\"\",\"spatSource\":\"V2X\",\"originIp\":\"10.11.81.12\",\"isCertPresent\":false},\"payload\":{\"data\":{\"timeStamp\":null,\"name\":null,\"intersectionStateList\":null},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735SPAT\"}}";
-
-        var deserialized = (OdeSpatData)JsonUtils.fromJson(bsmJson, OdeSpatData.class);
+        final var deserialized = (OdeSpatData)JsonUtils.fromJson(json, OdeSpatData.class);
         assertNotNull(deserialized);
         assertTrue(deserialized.getMetadata() instanceof OdeSpatMetadata);
         assertTrue(deserialized.getPayload() instanceof OdeSpatPayload);
@@ -20,11 +20,8 @@ public class OdeSpatDataTest {
 
     @Test
     public void serializationShouldNotAddClassProperty() {
-
-        final String json = "{\"metadata\":{\"logFileName\":\"\",\"recordType\":\"spatTx\",\"securityResultCode\":\"success\",\"receivedMessageDetails\":{\"locationData\":null,\"rxSource\":\"NA\"},\"encodings\":null,\"payloadType\":\"us.dot.its.jpo.ode.model.OdeSpatPayload\",\"serialId\":{\"streamId\":\"24d9dfcb-1ca0-41b8-8576-f4a00a51218e\",\"bundleSize\":1,\"bundleId\":0,\"recordId\":0,\"serialNumber\":0},\"odeReceivedAt\":\"2022-06-17T19:02:05.366148Z\",\"schemaVersion\":6,\"maxDurationTime\":0,\"recordGeneratedAt\":\"\",\"recordGeneratedBy\":null,\"sanitized\":false,\"odePacketID\":\"\",\"odeTimStartDateTime\":\"\",\"spatSource\":\"V2X\",\"originIp\":\"10.11.81.12\",\"isCertPresent\":false},\"payload\":{\"data\":{\"timeStamp\":null,\"name\":null,\"intersectionStateList\":null},\"dataType\":\"us.dot.its.jpo.ode.plugin.j2735.J2735SPAT\"}}";
-
-        var deserialized = (OdeSpatData)JsonUtils.fromJson(json, OdeSpatData.class);
-        String serialized = JsonUtils.toJson(deserialized, false);
+        final var deserialized = (OdeSpatData)JsonUtils.fromJson(json, OdeSpatData.class);
+        final String serialized = deserialized.toJson(false);
         assertFalse(serialized.contains("@class"));
     }
 }
