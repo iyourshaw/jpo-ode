45766b50451019d3aed7e922b66d0f9c6435cd55
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-05-14 14:23:27 -0600

    All udp message receivers are working

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/coder/OdeMessageFrameDataCreatorHelper.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/coder/OdeMessageFrameDataCreatorHelper.java
@@ -1,15 +1,10 @@
 package us.dot.its.jpo.ode.coder;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
-import us.dot.its.jpo.asn.j2735.r2024.MapData.MapData;
-import us.dot.its.jpo.asn.j2735.r2024.MapData.MapDataMessageFrame;
 import us.dot.its.jpo.asn.j2735.r2024.MessageFrame.MessageFrame;
 import us.dot.its.jpo.ode.model.OdeMessageFrameData;
 import us.dot.its.jpo.ode.model.OdeMessageFrameMetadata;
@@ -18,16 +13,16 @@ import us.dot.its.jpo.ode.model.OdeMsgMetadata;
 
 import us.dot.its.jpo.ode.model.ReceivedMessageDetails;
 import us.dot.its.jpo.ode.model.RxSource;
-//import us.dot.its.jpo.ode.util.JsonUtils;
+import us.dot.its.jpo.ode.util.JsonUtils;
 import us.dot.its.jpo.ode.util.XmlUtils;
-//import us.dot.its.jpo.ode.util.XmlUtils.XmlUtilsException;
+
 
 @Slf4j
 public class OdeMessageFrameDataCreatorHelper {
 
   public static OdeMessageFrameData createOdeMessageFrameData(String consumedData) throws JsonProcessingException {
     log.debug("createOdeMessageFrameData");
-    ObjectNode consumed = xmlMapper.readValue(consumedData, ObjectNode.class);
+    ObjectNode consumed = XmlUtils.getPlainXmlMapper().readValue(consumedData, ObjectNode.class);
     log.debug("consumed");
 
     JsonNode metadataNode = consumed.findValue(OdeMsgMetadata.METADATA_STRING);
@@ -42,7 +37,7 @@ public class OdeMessageFrameDataCreatorHelper {
 
       JsonNode jsonNode;
       try {
-        jsonNode = objectMapper.readTree(receivedMessageDetails.toJson());
+        jsonNode = JsonUtils.getPlainMapper().readTree(receivedMessageDetails.toJson());
         object.set(OdeMsgMetadata.RECEIVEDMSGDETAILS_STRING, jsonNode);
       } catch (IOException e) {
         log.error("Failed to parse receivedMessageDetails", e);
@@ -51,7 +46,7 @@ public class OdeMessageFrameDataCreatorHelper {
 
     }
 
-    OdeMessageFrameMetadata metadata = objectMapper.convertValue(metadataNode, OdeMessageFrameMetadata.class);
+    OdeMessageFrameMetadata metadata = XmlUtils.getPlainXmlMapper().convertValue(metadataNode, OdeMessageFrameMetadata.class);
     log.debug("got messageframe metadata");
 
     if(metadataNode.findValue("certPresent") != null) {
@@ -67,21 +62,11 @@ public class OdeMessageFrameDataCreatorHelper {
     log.debug("after schema version check");
 
 
-//    JsonNode messageFrameNode = consumed.findValue("MessageFrame");
-//    log.debug("got messageframe node");
+    JsonNode messageFrameNode = consumed.findValue("MessageFrame");
+    log.debug("got messageframe node");
 
-    final String startTag = "<MessageFrame>";
-    final String endTag = "</MessageFrame>";
-    int startIndex = consumedData.indexOf(startTag);
-    int endIndex = consumedData.indexOf(endTag);
-    final String mfString = consumedData.substring(startIndex, endIndex + endTag.length());
+    MessageFrame<?> messageFrame = XmlUtils.getPlainXmlMapper().convertValue(messageFrameNode, MessageFrame.class);
 
-
-    //final String mfString = XmlUtils.getPlainXmlMapper().writeValueAsString(messageFrameNode);
-    log.debug("got mf string: {}", mfString);
-
-    MessageFrame<?> messageFrame = xmlMapper.readValue(mfString, MessageFrame.class);
-    //messageFrame = new MapDataMessageFrame();
     log.debug("got message frame");
 
     OdeMessageFramePayload payload = new OdeMessageFramePayload(messageFrame);
@@ -89,7 +74,6 @@ public class OdeMessageFrameDataCreatorHelper {
     return new OdeMessageFrameData(metadata, payload);
   }
 
-  private final static ObjectMapper objectMapper = new ObjectMapper();
-  private final static XmlMapper xmlMapper = new XmlMapper();
+
 
 }
